3726acec36086ab4c74c51791d9f0272b160d584
lengary1110 <ziyuanx1@student.unimelb.edu.au>
2021-04-26 11:18:32 +0800

    CCC-2: add app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
+
+
+# Error Handling
+@app.errorhandler(400)
+def bad_request():
+    return make_response(jsonify({'error': 'Bad request'}), 400)
 
 
 @app.route('/')
@@ -8,9 +14,25 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/scenarios', methods=['GET'])
+@app.route('/api/v1/scenarios', methods=['GET'])
 def get_scenarios():
-    return 'There are some scenarios!'
+    return jsonify({'scenarios': scenarios})
+
+
+scenarios = [
+    {
+        'id': 1,
+        'location': u'Melbourne',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Sydney',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
 
 
 if __name__ == '__main__':
