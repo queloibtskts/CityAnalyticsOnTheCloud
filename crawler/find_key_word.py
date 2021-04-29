2f4e373a1e98b53ef1137a5ccabc67a6b51c4865
erichong0815 <67636157+erichong0815@users.noreply.github.com>
2021-04-29 22:23:52 +0800

    Add files via upload

--- crawler/find_key_word.py
+++ crawler/find_key_word.py
@@ -9,7 +9,7 @@ with open("word.txt", 'r') as f:
         line = line.strip('\t').strip()
         text_list.append(line)
 
-count = 1
+id_list = []
 with open("tweets.json", 'r', encoding="utf-8") as f:
     for line in f:
         line = line.rstrip()
@@ -19,12 +19,16 @@ with open("tweets.json", 'r', encoding="utf-8") as f:
         
 
         data = json.loads(line)
-        text = data.split('"text":')[-1]
-        text = data.split(',"display_text_range":')[0]
-
+        text = data.split('"text":',1)[-1]
+        text = text.split(',"source":"')[0]
         text = text.lower().split()
         
-     
+        id_str = data.split('"id_str":',1)[-1]
+        id_str = id_str.split(',"text":')[0]
+        
+        if (id_str in id_list):
+        	continue
+
         for i in range(len(text)):
             if((text[i]+' '+text[(i+1)%len(text)]+' '+text[(i+2)%len(text)]+' '+text[(i+3)%len(text)]+' '+text[(i+4)%len(text)]) in text_list and is_in==False):
                 print(data)
@@ -41,3 +45,4 @@ with open("tweets.json", 'r', encoding="utf-8") as f:
             elif((text[i]) in text_list and is_in==False):
                 print(data)
                 is_in = True
+        id_list.append(id_str)
