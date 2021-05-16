7f046350a35007aaad61ba2cfa2ad3a21eec61fc
erichong0815 <67636157+erichong0815@users.noreply.github.com>
2021-05-16 19:11:56 +0800

    Update streaming_to_couchdb.py

--- crawler/streaming_to_couchdb.py
+++ crawler/streaming_to_couchdb.py
@@ -1,68 +1,11 @@
-#from __future__ import absolute_import, print_function
 import tweepy
-from tweepy import OAuthHandler, Stream, StreamListener
 import json
-# pip install couchdb
 import couchdb
+import time
+import preprocessor as p
+p.set_options(p.OPT.URL, p.OPT.MENTION)  # remove URL and mentions in tweet text
 
-
-# Twitter api auth
-consumer_key = "gAJAEIYlILzQuCpdEDKnIeLrj"
-consumer_secret = "xHpwWfCFeHGAKFc6w94Kk5TFsWqlgYcqt82v1sFqbY0yYMt9am"
-access_token = "1382217118658142211-uTL6uM6alWIAlCmErC12zuY3xQOAAI"
-access_token_secret = "vTqn2fUSwH31aRDsS3kYvDKLuLEQZIGmtfDL57E1ba92N"
-
-# couch db auth
-database = 'ccc_twitter_test6'  # australia any realtime tweet in english
-server = "localhost:5984"
-host = ''  # for remote db
-port = ''  # for remote db
-admin_username = "admin"
-admin_password = "12354"
-
-locations = [110.01, -45.02, 160.82, -12.02]  # Australia
-# filterTerms = [" "]
-WARNING_COLOUR = '\033[93m'
-END_COLOUR = '\033[0m'
-
-
-class CouchDBStreamListener(StreamListener):
-    # ref: https://pmatigakis.wordpress.com/2012/12/01/twitter-data-mining-crawling-twitter/
-    def __init__(self, db):
-        self.db = db
-        self.tweet_count = 0
-        # self.received_friend_ids = False
-    
-    def on_data(self, data):
-        try:
-            tweet = json.loads(data)
-        except Exception:
-            print(WARNING_COLOUR + "Failed to parse tweet data" + END_COLOUR)
-            tweet = None
-        
-        if tweet:
-            if tweet.__contains__('id') and tweet.__contains__("text"):
-                print(tweet)
-                
-                tweet['doc_type'] = "tweet"  # partition key?
-                
-                self.db["tweet:%d" % tweet['id']] = tweet
-                
-                self.tweet_count += 1
-            # elif not self.received_friend_ids and tweet.__contains__("friends"):  # ?
-            #     print("Got %d user ids" % len(tweet['friends']))  # ?
-            #     self.received_friend_ids = True  # ?
-            else:
-                print(
-                    WARNING_COLOUR + "Received a response that is not a tweet" + END_COLOUR + " :      " + tweet)
-        return True
-
-
-def connect_to_couch_db_server(host, port, username, password):
-    secure_remote_server = couchdb.Server(
-        'http://' + username + ':' + password + '@' + host + ':' + port)
-    return secure_remote_server
-
+#db = server['tweets']
 
 def connect_to_database(database_name, server):
     try:
@@ -70,21 +13,108 @@ def connect_to_database(database_name, server):
     except:
         return server.create(database_name)
 
+database1 = 'vulgar_tweet_by_search'
+database2 = 'clean_tweet_by_search'
 
-def main():
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    
-    # connect to couch db
-    server = couchdb.Server()
-    server.resource.credentials = (admin_username, admin_password)
-    # server = connect_to_couch_db_server(host, port, admin_username, admin_password)
-    db = connect_to_database(database, server)
-    
-    listener = CouchDBStreamListener(db)
-    
-    stream = Stream(auth, listener)
-    stream.filter(locations = locations, languages = ["en"], is_async = True)
+# uncomment 5 lines below to connect to local db server
+# server = "localhost:5984"
+# admin_username = "admin"
+# admin_password = "12354"
+# server = couchdb.Server()
+# server.resource.credentials = (admin_username, admin_password)
 
-if __name__=="__main__":
-    main()
+# connect to remote db server; comment 1 line below if connecting to local db server
+server = couchdb.Server('http://admin:12345@127.0.0.1:5984/')
+
+db1 = connect_to_database(database1, server)
+db2 = connect_to_database(database2, server)
+
+
+consumer_key = "gAJAEIYlILzQuCpdEDKnIeLrj"
+consumer_secret = "xHpwWfCFeHGAKFc6w94Kk5TFsWqlgYcqt82v1sFqbY0yYMt9am"
+access_token = "1382217118658142211-uTL6uM6alWIAlCmErC12zuY3xQOAAI"
+access_token_secret = "vTqn2fUSwH31aRDsS3kYvDKLuLEQZIGmtfDL57E1ba92N"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit = True)
+
+
+
+
+
+text_list = []
+
+with open("swear_word.txt", 'r') as f:
+    for line in f:
+        line = line.strip('\t').strip()
+        text_list.append(line)
+
+
+
+
+
+
+class CouchDBStreamListener(tweepy.StreamListener):
+    def on_data(self, data):
+        try:
+            text = json.loads(data)['text']
+            text = p.clean(text)
+            text = text.lower().split()
+            is_in = False
+            vulgar_words_used = []
+            for i in range(len(text)):
+                if((text[i]+' '+text[(i+1)%len(text)]+' '+text[(i+2)%len(text)]+' '+text[(i+3)%len(text)]+' '+text[(i+4)%len(text)]) in text_list):
+                    is_in = True
+                    vulgar_words_used.append(text[i]+' '+text[(i+1)%len(text)]+' '+text[(i+2)%len(text)]+' '+text[(i+3)%len(text)]+' '+text[(i+4)%len(text)])
+                
+                elif((text[i]+' '+text[(i+1)%len(text)]+' '+text[(i+2)%len(text)]+' '+text[(i+3)%len(text)]) in text_list):
+                    is_in = True
+                    vulgar_words_used.append(text[i]+' '+text[(i+1)%len(text)]+' '+text[(i+2)%len(text)]+' '+text[(i+3)%len(text)])
+                
+    
+                elif((text[i]+' '+text[(i+1)%len(text)]+' '+text[(i+2)%len(text)]) in text_list):
+                    is_in = True
+                    vulgar_words_used.append(text[i]+' '+text[(i+1)%len(text)]+' '+text[(i+2)%len(text)])
+    
+    
+                elif((text[i]+' '+text[(i+1)%len(text)]) in text_list):
+                    is_in = True
+                    vulgar_words_used.append(text[i]+' '+text[(i+1)%len(text)])
+    
+    
+                elif((text[i]) in text_list):
+                    is_in = True
+                    vulgar_words_used.append(text[i])
+    
+            data = json.loads(data)
+            if is_in:
+                data['tag'] = {'vulgar_words': "True", 'vulgar_words_used': vulgar_words_used}
+                db1["tweet:%d" % data['id']] = data # error caused by saving json.dumps(data) to db
+                #print(json.dumps(data))
+            else:
+                data['tag'] = {'vulgar_words': "False", 'vulgar_words_used': vulgar_words_used}
+                db2["tweet:%d" % data['id']] = data # error caused by saving json.dumps(data) to db
+                #print(json.dumps(data))
+
+
+        except BaseException as e:
+            print("BaseException occurs!!",e)
+            print(data)
+            return True
+        return True
+
+    def on_error(self, status_code):
+        print(status_code,"error")
+        return True
+
+    def on_timeout(self):
+        print("timeout")
+        return True 
+while True:
+    try:
+        stream = tweepy.streaming.Stream(auth, CouchDBStreamListener())
+        stream.filter(locations=[110.01,-45.02,160.82,-12.02],languages=['en'])
+    except:
+        print("error")
+        time.sleep(30)
