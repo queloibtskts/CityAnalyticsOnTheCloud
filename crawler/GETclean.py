5890448815bf0e4c511b9f67739dd5c0ecf0efc8
erichong0815 <67636157+erichong0815@users.noreply.github.com>
2021-05-19 11:50:53 +0800

    Add files via upload

---
+++ crawler/GETclean.py
@@ -0,0 +1,27 @@
+import couchdb
+import tweepy
+import json
+import couchdb
+import time
+import preprocessor as p
+import pandas as pd
+
+def connect_to_database(database_name, server):
+    try:
+        return server[database_name]
+    except:
+        return server.create(database_name)
+
+server = couchdb.Server('http://admin:12345@127.0.0.1:5984/')
+
+db = connect_to_database("vulgar_tweet_by_search", server)
+#print(db["37c1fde588eef4e9ca254c06b2058579"])
+rows = db.view('_all_docs', include_docs=True)
+data = [row['doc'] for row in rows]
+for i in range(len(data)):
+    temp = data[0]
+    temp = json.dumps(temp)
+    temp = json.loads(temp)
+    temp.pop('_id', None)
+    temp.pop('_rev', None)
+    print(temp)
---
+++ crawler/toDB.py
@@ -0,0 +1,30 @@
+import json
+import tweepy
+import json
+import couchdb
+
+def connect_to_database(database_name, server):
+    try:
+        return server[database_name]
+    except:
+        return server.create(database_name)
+
+
+server = couchdb.Server('http://admin:12345@127.0.0.1:5984/')
+
+db = server['tweets']
+database1 = 'vulgar_tweet_by_search'
+database2 = 'clean_tweet_by_search'
+
+db1 = connect_to_database(database1, server)
+db2 = connect_to_database(database2, server)
+
+
+
+with open("tag_tweets_keyword.json", 'r') as f:
+    for line in f:
+        data = json.loads(line)
+        if(data["tag"]["vulgar_words"] == "True"):
+            db1.save(data)
+        elif(data["tag"]["vulgar_words"] == "False"):
+            db2.save(data)
