ec3986a925867000669ff7596ec8dc0c39be1ff3
Xiaoyue Lin <xiaoyuel4@student.unimelb.edu.au>
2021-05-21 13:09:23 +1000

    Update utils.py

--- backend/utils.py
+++ backend/utils.py
@@ -1,15 +1,28 @@
-def view_reformatter(rows):
-    all_freqs = {}
-    for row in rows:
+def view_reformatter(rows, URL):
+    if URL[-2:] == 'AU':
+        # mapreduce the whole country
         freqs = []
-        for (k, v) in row['value'].items():
-            freqs.append({'text': k, 'value': v})
-        all_freqs[row['key']] = freqs
-    return all_freqs
+        for row in rows:
+            freqs.append({'text': row['key'], 'value': row['value']})
+        all_freqs = {'AU': freqs}
+        return all_freqs
+    else:
+        # mapreduce by states
+        all_freqs = {}
+        for row in rows:
+            freqs = []
+            for (k, v) in row['value'].items():
+                freqs.append({'text': k, 'value': v})
+            all_freqs[row['key']] = freqs
+        return all_freqs
 
-def view_reformatterAU(rows):
-    freqs = []
+def getTop3VulgarWords(rows):
+    newrows = []
     for row in rows:
-        freqs.append({'text': row['key'], 'value': row['value']})
-    all_freqs = {'AU': freqs}
-    return all_freqs
+        newfreq = {}
+        freq = row['value']
+        top3Freq = {k: freq[k] for k in sorted(freq, key=lambda x: freq[x], reverse=True)[:3]}
+        newfreq['key'] = row['key']
+        newfreq['value'] = top3Freq
+        newrows.append(newfreq)
+    return newrows
