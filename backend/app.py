0d22a497af8838106b51c6905c2a8380248b9861
Xiaoyue Lin <xiaoyuel4@student.unimelb.edu.au>
2021-05-21 13:10:09 +1000

    Add views

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify, make_response
 import couchdb
 from flask_cors import CORS
 from utils import view_reformatter
-from utils import view_reformatterAU
+from utils import getTop3VulgarWords
 cache = redis.Redis(host='redis', port=6379)
 
 app = Flask(__name__)
@@ -24,7 +24,30 @@ vulgardb = connect_to_database(vulgarDBNAME, server)
 cleanDBNAME = 'clean_tweet_by_search'
 cleandb = connect_to_database(cleanDBNAME, server)
 
-vulgarWordFreq = vulgardb.view('language/vulgarWordFreq', group=True)
+URL_vulgarWordFreq = 'language/vulgarWordFreq'
+URL_vulgarWordFreqAU = 'language/vulgarWordFreqAU'
+URL_hashtagFreq = 'language/hashtagFreq'
+URL_hashtagFreqAU = 'language/hashtagFreqAU'
+URL_countTweetByStates = 'language/countTweetByStates'
+
+# Get rows of views
+vulgarWordFreq = view_reformatter(vulgardb.view(URL_vulgarWordFreq, group=True).rows,
+                                  URL_vulgarWordFreq) # vulgar word frequency in each state
+vulgarWordFreqTop3 = view_reformatter(getTop3VulgarWords(
+                                vulgardb.view(URL_vulgarWordFreq, group=True).rows),
+                                URL_vulgarWordFreq) # top3 vulgar word frequency in each state
+vulgarWordFreqAU = view_reformatter(vulgardb.view(URL_vulgarWordFreqAU, group=True).rows,
+                                    URL_vulgarWordFreqAU) # vulgar word frequency in australia
+vulgar_viewHashtagFreq = view_reformatter(vulgardb.view(URL_hashtagFreq, group=True).rows,
+                            URL_hashtagFreq) # hashtag frequency in vulgar tweets from each state
+clean_viewHashtagFreq = view_reformatter(cleandb.view(URL_hashtagFreq, group=True).rows,
+                            URL_hashtagFreq) # hashtag frequency in clean tweets from each state
+vulgar_viewHashtagFreqAU = view_reformatter(vulgardb.view(URL_hashtagFreqAU, group=True).rows,
+                            URL_hashtagFreqAU) # hashtag frequency in vulgar tweets in australia
+clean_viewHashtagFreqAU = view_reformatter(cleandb.view(URL_hashtagFreqAU, group=True).rows,
+                            URL_hashtagFreqAU) # hashtag frequency in clean tweets in australia
+vulgar_countTweetByStates = vulgardb.view(URL_countTweetByStates, group=True) # vulgar tweet count in each state
+clean_countTweetByStates = vulgardb.view(URL_countTweetByStates, group=True) # clean tweet count in each state
 
 mocks_scenario2 = {
     "title":['State', 'TOP1', 'TOP2', 'TOP3'],
@@ -40,22 +63,18 @@ mocks_scenario2 = {
 def get_scenario_two():
     return jsonify(mocks_scenario2)
 
-vulgarWordFreqAU = vulgardb.view('language/vulgarWordFreqAU', group=True)
-
 @app.route('/scenario3', methods=['GET'])
 def get_scenario_three():
-    return jsonify(view_reformatterAU(vulgarWordFreqAU.rows))
-
-vulgar_viewHashtagFreq = vulgardb.view('language/hashtagFreq', group=True)
-clean_viewHashtagFreq = cleandb.view('language/hashtagFreq', group=True)
+    return jsonify(vulgarWordFreq)
 
 @app.route('/scenario4/vulgar', methods=['GET'])
 def get_scenario_four_vulgar():
-    return jsonify(view_reformatter(vulgar_viewHashtagFreq.rows))
+    return jsonify(vulgar_viewHashtagFreq)
 
 @app.route('/scenario4/clean', methods=['GET'])
 def get_scenario_four_clean():
-    return jsonify(view_reformatter(clean_viewHashtagFreq.rows))
+    return jsonify(clean_viewHashtagFreq)
+
 # Error Handling
 @app.errorhandler(400)
 def bad_request():
